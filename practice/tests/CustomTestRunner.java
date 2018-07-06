3e480703b589e7055527474edf29678532ee4be6
Alexey Makarenko <amakarenko@wiley.com>
2018-07-06 10:11:16 +0300

    1

--- practice/tests/CustomTestRunner.java
+++ practice/tests/CustomTestRunner.java
@@ -1,14 +1,9 @@
-import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import static dataTypes.dataTypes.DATA_TYPES;
 
 public class CustomTestRunner {
 
     @Test
     public void runTest() {
-        Assert.assertEquals(DATA_TYPES, "DATA_TYPES");
-        Assert.assertEquals(0, 0);
-        System.out.print("OK");
+
     }
 }
