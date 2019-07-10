dc6c681187e815b88b285ceb58cbfd789da51307
StumpKeeper <alexeymkr@gmail.com>
2019-07-10 22:49:36 +0300

    holder class converted to kotlin

--- practice/src/selenium/WebDriverHolder.kt
+++ practice/src/selenium/WebDriverHolder.kt
@@ -3,43 +3,32 @@ package selenium
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
-import org.testng.Assert
 import selenium.ChromeDriverMode.HEADLESS
 import selenium.ChromeDriverMode.NORMAL
 import java.io.File
 import java.io.IOException
 
 object WebDriverHolder {
-    private var webDriver: WebDriver? = null
-    private const val DRIVER_INIT_ERROR_MESSAGE: String =
-            "WebDriver was not initialized, use WebDriverHolder.init() method before calling getDriver()"
+    var webDriver: WebDriver? = null
 
-    fun getDriver(): WebDriver? {
-        Assert.assertNotNull(webDriver, DRIVER_INIT_ERROR_MESSAGE)
-        return webDriver
-    }
-
-    /**
-     * Initializes ChromeDriver with custom driver mode
-     *
-     * @param chromeDriverMode - desired driver mode enum
-     */
     fun init(chromeDriverMode: ChromeDriverMode = NORMAL) {
         if (webDriver == null) {
             setDriverBinaryPaths()
-            webDriver = if (chromeDriverMode == HEADLESS) {
-                val chromeOptions = ChromeOptions()
-                chromeOptions.addArguments("--headless", "--disable-gpu", "--disable-extensions", "window-size=1920x1080")
-                ChromeDriver(chromeOptions)
-            } else
-                ChromeDriver()
+            webDriver = when (chromeDriverMode) {
+                NORMAL -> {
+                    ChromeDriver()
+                }
+                HEADLESS -> {
+                    val chromeOptions = ChromeOptions()
+                    chromeOptions.addArguments("--headless", "--disable-gpu", "--disable-extensions", "window-size=1920x1080")
+                    ChromeDriver(chromeOptions)
+                }
+            }
         }
     }
 
     fun closeDriver() {
-        if (webDriver != null) {
-            webDriver!!.close()
-        }
+        webDriver?.close()
     }
 
     private fun setDriverBinaryPaths() {
