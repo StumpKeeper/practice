0b72d0d75706f4d2be4b8b7c6b23f36b3a8b17be
StumpKeeper <alexeymkr@gmail.com>
2019-11-01 19:30:08 +0300

    toString added to GuitarString class

--- practice/src/org/acme/grabber/entities/GuitarString.kt
+++ practice/src/org/acme/grabber/entities/GuitarString.kt
@@ -1,5 +1,11 @@
 package org.acme.grabber.entities
 
-class GuitarString(val note: Note,
-                   val size: StringSize,
-                   var tension: String = "")
+class GuitarString(
+        val note: Note,
+        val size: StringSize,
+        var tension: String = "") {
+
+    override fun toString(): String {
+        return String.format("%s - %s - %s", this.note, this.size, this.tension)
+    }
+}
