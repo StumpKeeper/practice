dca7ef5151978dd0eb974bd7ee86defde5274f16
StumpKeeper <alexeymkr@gmail.com>
2023-12-16 12:16:35 +0300

    - main method optimized - added manual system exit after calculations is complete

--- practice/src/Main.kt
+++ practice/src/Main.kt
@@ -3,18 +3,15 @@ import org.acme.grabber.entities.Tuning
 import org.acme.grabber.entities.VendorStringSet
 import org.acme.selenium.WebDriverHolder
 
-object Main {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val stringSet = TunedStringSet(Tuning.DROP_D, VendorStringSet.GE_09_46)
+fun main() {
+    val tunedStringSet = TunedStringSet(Tuning.DROP_D, VendorStringSet.EB_10_52)
 
-        stringSet.apply {
-            calculateTension()
-            prettyPrint()
-        }
-
-        WebDriverHolder.webDriver.close()
-
-        readLine()
+    tunedStringSet.apply {
+        calculateTension()
+        prettyPrint()
     }
+
+    WebDriverHolder.webDriver.close()
+
+    System.exit(0)
 }
